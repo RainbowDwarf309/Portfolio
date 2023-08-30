1c59c95b74d737d398e64fd565c89ee1c1e8c4f0
rainbowdwarf <glubokov3099@gmail.com>
2023-08-30 13:54:18 +0300

    first fix to pc model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -62,11 +62,11 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        {/*<OrbitControls*/}
+        {/*  enableZoom={false}*/}
+        {/*  maxPolarAngle={Math.PI / 2}*/}
+        {/*  minPolarAngle={Math.PI / 2}*/}
+        {/*/>*/}
         <Computers isMobile={isMobile} />
       </Suspense>
 
